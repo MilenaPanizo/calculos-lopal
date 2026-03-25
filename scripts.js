0f78bd5d2a30c53009a6774ee3e9709753b7b4a1
I1HN <analiviaa.0015@gmail.com>
2026-03-25 08:50:17 -0300

    Criando a multiplicação

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ let numUm
 let numDois
 let result
 
-numUm = Number(prompt("Digite o primeiro numero"));
-numDois = Number(prompt("Digite o segundo numero"));
+numUm = Number(prompt("Digite o primeiro número"));
+numDois = Number(prompt("Digite o segundo número"));
 
 result = numUm*numDois
 
-alert ("O resultado da divisão é " + result);
+alert ("O resultado da multiplicação é " + result);
