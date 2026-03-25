351356d366a5aeb6293abd521cc2a134a63fb1f2
MilenaPanizo <milena.ap.pn.nascimento@gmail.com>
2026-03-25 08:13:19 -0300

    Iniciando atividade Maycon

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let numUm
+let numDois
+let result
+
+numUm = Number(prompt("Digite o primeiro numero"));
+numDois = Number(prompt("Digite o segundo numero"));
+
+result = numUm/numDois
+
+alert ("O resultado da divisão é " + result);
